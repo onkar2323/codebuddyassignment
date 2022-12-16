e4c3ca2e98ea35aa75f0e144149e756158ec8d4d
onkar2323 <onkarsetia23@gmail.com>
2022-12-16 16:43:44 +0530

    assignment completed

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,62 @@
 const User = require('../schema/user.schema');
+const Post = require('../schema/post.schema')
 
 module.exports.getUsersWithPostCount = async (req, res) => {
+    const getCount = async (id) => {
+        return await Post.find({userId: id}).count()
+    }
+
     try {
+
+        var us = await User.find({}, {__v: 0}).count();
+        
+        const totalDocs = us;
+        const page = req.query.page || 0;
+        let prevPage = page - 1;
+        let nextPage = page + 1;
+        const limit = req.query.limit || 10;
+        const totalPages = totalDocs/limit;
+        if(prevPage < 0){
+            prevPage = null;
+        }
+        if(nextPage > totalPages){
+            nextPage = null;
+        }
+        var hasPrevPage = true;
+        var hasNextPage = true;
+        if(page<2){
+            hasPrevPage = false;
+        }
+        else if(page>totalPages){
+            hasNextPage = false;
+        } 
+        
+        var uss = await User.find({}, {__v: 0}).limit(limit).skip(page * limit);
         //TODO: Implement this API
+        const updatedUs =await Promise.all(uss.map(async (ele) => {
+            return {...ele._doc, posts: await getCount(ele._id) }
+        }))
+       
+
+
         res.status(200).json({
-            message: 'Implement this API'
+            data: {
+                "users": updatedUs,
+                "pagination":{
+                    "total docs": totalDocs,
+                    "page": page,
+                    "limit": limit,
+                    "totalPages": totalPages,
+                    "pagingCounter": page,
+                    "hasPrevPage": hasPrevPage,
+                    "hasNextPage": hasNextPage,
+                    "prevPage": prevPage,
+                    "nextPage": nextPage
+
+                }
+            },
+
+            
         })
     } catch (error) {
         res.send({error: error.message});
